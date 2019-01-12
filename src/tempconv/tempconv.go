cfaf821580609fc11a3e766b1281d295e949ce8b
Jozef Reisinger <jozef.reisinger@telekom.com>
2019-01-12 15:32:37 +0100

    update file src/tempconv/tempconv.go

--- src/tempconv/tempconv.go
+++ src/tempconv/tempconv.go
@@ -3,15 +3,18 @@ package tempconv
 
 import "fmt"
 
+// Types declarations.
 type Celsius float64
 type Fahrenheit float64
 
+// Constants declarations.
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
 )
 
+// Conversion functions.
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
